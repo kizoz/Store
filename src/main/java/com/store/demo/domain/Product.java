f40525fbdcc8fcdc0204a44aefc1b54bddde4792
kizoz <sasha.kizevich@yandex.ru>
2020-05-22 17:12:00 +0300

    fixed imports

--- src/main/java/com/store/demo/domain/Product.java
+++ src/main/java/com/store/demo/domain/Product.java
@@ -1,10 +1,5 @@
 package com.store.demo.domain;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonIdentityReference;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-
 import javax.persistence.*;
 import java.util.List;
 
